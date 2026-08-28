d96b0e4677c879e328f2d6ff3f1a16352946c4ac
ALI-Yah1a <aliyahia1230123@gmail.com>
2026-08-28 18:31:01 +0300

    Adding a level complete UI

--- Scripts/level_1.gd
+++ Scripts/level_1.gd
@@ -2,12 +2,16 @@ extends Node2D
 
 @onready var player: CharacterBody2D = $Player
 @onready var zombies_label: Label = $CanvasLayer/ZombiesLabel
-
+@onready var level_complete_ui: CanvasLayer = $LevelCompleteUI
+@onready var next_level_button: Button = $LevelCompleteUI/HBoxContainer/NextLevelButton
+@onready var main_menu_button: Button = $LevelCompleteUI/HBoxContainer/MainMenuButton
 var zombies_killed = 0
-const REQUIRED_ZOMBIES = 10
+const REQUIRED_ZOMBIES = 1
 
 func _ready():
-
+	level_complete_ui.visible = false
+	next_level_button.pressed.connect(_on_next_level_button_pressed)
+	main_menu_button.pressed.connect(_on_main_menu_button_pressed)
 	update_ui()
 
 func update_ui():
@@ -19,4 +23,14 @@ func zombie_killed():
 	level_finished()
 func level_finished():
 	if zombies_killed >= REQUIRED_ZOMBIES:
-		get_tree().change_scene_to_file("res://UI/main_menu.tscn")
+		get_tree().paused = true
+		level_complete_ui.visible = true
+
+func _on_next_level_button_pressed() -> void:
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://Scenes/level_2.tscn")
+
+
+func _on_main_menu_button_pressed() -> void:
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://UI/main_menu.tscn")
